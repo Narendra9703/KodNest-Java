98457a3202f57a2d0ddfa0c0c8592a793e5f86af
Narendra kumar <narendrakumarjangam2@gmail.com>
2026-08-04 11:33:32 +0530

    Added Simple Program

---
+++ track/Practise/Example.java
@@ -0,0 +1,9 @@
+package track.Practise;
+
+public class Example {
+    public static void main(String[] args) {
+
+        System.out.println("Monday \nTuesdayWednesday \n Thursday \n FridaySaturday \n Sunday ");
+
+    }
+}
