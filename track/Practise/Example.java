d74c44232dfba84c45afb9dd7d49955f48ccc6a3
Narendra kumar <narendrakumarjangam2@gmail.com>
2026-08-04 11:45:22 +0530

    Added Simple Program

--- track/Practise/Example.java
+++ track/Practise/Example.java
@@ -4,11 +4,11 @@ public class Example {
     public static void main(String[] args) {
 
         System.out.println("Monday ");
-        System.err.println("Tuesday");
+        System.err.print("Tuesday");
         System.out.println("wednesday");
         System.out.println("Thursday");
-        System.out.println("Friday");
+        System.out.print("Friday");
         System.out.println("Saturday");
-        System.out.println("Sunday");
+        System.out.print("Sunday");
     }
 }
