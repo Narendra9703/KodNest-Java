053f7fe67e30be318bd61be42efa9cf78157ed3f
Narendra kumar <narendrakumarjangam2@gmail.com>
2026-08-04 11:38:38 +0530

    Added Simple Program

--- track/Practise/Example.java
+++ track/Practise/Example.java
@@ -3,7 +3,12 @@ package track.Practise;
 public class Example {
     public static void main(String[] args) {
 
-        System.out.println("Monday \nTuesdayWednesday \n Thursday \n FridaySaturday \n Sunday ");
-
+        System.out.println("Monday ");
+        System.err.println("Tuesday");
+        System.out.println("wednesday");
+        System.out.println("Thursday");
+        System.out.println("Friday");
+        System.out.println("Saturday");
+        System.out.println("Sunday");
     }
 }
