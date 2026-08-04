cc5568ea74deff9a3bba23ef083011337877ec01
Narendra kumar <narendrakumarjangam2@gmail.com>
2026-08-04 12:33:54 +0530

    Added checking Number

---
+++ track/Practise/Example1.java
@@ -0,0 +1,16 @@
+package track.Practise;
+
+public class Example1 {
+    public static void main(String[] args) {
+        int num = 25;
+        if (num > 0) {
+            System.out.println(num + " is number is positive");
+        } else if (num < 0) {
+            System.out.println(num + " is number is nagative");
+        } else {
+            System.out.println(num + " is number is Zero");
+        }
+
+    }
+
+}
