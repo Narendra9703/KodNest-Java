d804a737f0c73f80550c560fd29bea452b22b614
Narendra kumar <narendrakumarjangam2@gmail.com>
2026-08-06 11:53:40 +0530

    Added for loop program

---
+++ track/Practise/Example3.java
@@ -0,0 +1,9 @@
+package track.Practise;
+
+public class Example3 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
