f6f0265a7a44da4e2ec84bfe2c2024c4c20cd602
Narendra kumar <narendrakumarjangam2@gmail.com>
2026-08-09 10:36:46 +0530

    Added Ternary Program

---
+++ track/MO2/TO2/Control_constructs/Terniary.java
@@ -0,0 +1,14 @@
+package track.MO2.TO2.Control_constructs;
+
+public class Terniary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =10;
+		int b = 5;
+		String res =a>b?"A is greater: "+ a:"B is greater: "+ b;
+		System.out.println(res);
+
+	}
+
+}
