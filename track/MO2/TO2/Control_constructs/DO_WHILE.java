94657aa428a78a326fca8f56400382e5ee216f00
Narendra kumar <narendrakumarjangam2@gmail.com>
2026-08-11 09:30:21 +0530

    Added do-while program

---
+++ track/MO2/TO2/Control_constructs/DO_WHILE.java
@@ -0,0 +1,12 @@
+package track.MO2.TO2.Control_constructs;
+
+public class DO_WHILE {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 5);
+    }
+
+}
