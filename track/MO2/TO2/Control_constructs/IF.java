5175b69d6047cced8ce4fadaf8590b9db6d488d8
Narendra kumar <narendrakumarjangam2@gmail.com>
2026-08-11 07:46:30 +0530

    Added if program

---
+++ track/MO2/TO2/Control_constructs/IF.java
@@ -0,0 +1,9 @@
+package track.MO2.TO2.Control_constructs;
+
+public class IF {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ track/Practise/Example5.java
@@ -0,0 +1,14 @@
+package track.Practise;
+
+public class Example5 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        int j = 10;
+        while (j <= 20) {
+            System.out.println(j);
+            j++;
+        }
+    }
+}
