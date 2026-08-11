88ca502566c2189785ef25b31dd3362b49832186
Narendra kumar <narendrakumarjangam2@gmail.com>
2026-08-11 07:50:16 +0530

    Added While loop program

---
+++ track/MO2/TO2/Control_constructs/WHILE.java
@@ -0,0 +1,11 @@
+package track.MO2.TO2.Control_constructs;
+
+public class WHILE {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
