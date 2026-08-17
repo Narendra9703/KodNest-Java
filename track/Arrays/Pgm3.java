081af1751a61452a885ac1678828d38526e9f763
Narendra kumar <narendrakumarjangam2@gmail.com>
2026-08-17 13:19:24 +0530

    Added Reverse Array program

--- track/Arrays/Pgm3.java
+++ track/Arrays/Pgm3.java
@@ -2,16 +2,16 @@ package track.Arrays;
 
 import java.util.Scanner;
 
-public class Pgm2 {
+public class Pgm3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int[] a = new int[5];
         System.out.println("Enter Array Elements: ");
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i <= a.length - 1; i++) {
             a[i] = scan.nextInt();
         }
-        System.out.println("Array elements are:");
-        for (int i = 0; i < 4; i++) {
+        System.out.println("Array Elememts are: ");
+        for (int i = a.length - 1; i >= 0; i--) {
             System.out.println(a[i]);
         }
     }
